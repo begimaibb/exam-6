93802e32a14e8f5f5eb530007dde5d6c547eb4a6
begimaib <begimai.bolotbekova>
2022-07-02 18:19:28 +0600

    Model is created, migrated, admin page is formed according to the model

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from webapp.models import Guest
+
+
+class GuestAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'email', 'text']
+    list_display_links = ['name']
+    list_filter = ['name']
+    search_fields = ['name', 'email']
+    fields = ['name', 'email', 'text', 'created_date', 'updated_date', 'status']
+    readonly_fields = ['created_date', 'updated_date']
+
+
+admin.site.register(Guest, GuestAdmin)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Guest(models.Model):
+    status_choices = [('active', 'Active'), ('blocked', 'Blocked')]
+    name = models.CharField(max_length=50, null=False, blank=False, verbose_name="Name")
+    email = models.EmailField(max_length=50, null=False, blank=False, verbose_name="Email")
+    text = models.TextField(max_length=100, null=False, blank=False, verbose_name="Text")
+    created_date = models.DateTimeField(auto_now_add=True, verbose_name="Created date")
+    updated_date = models.DateTimeField(auto_now=True, verbose_name="Updated date")
+    status = models.CharField(max_length=50, choices=status_choices, default='1')
+
+
+    def __str__(self):
+        return f"{self.id}. {self.name}, {self.email} {self.text}"
+
+    class Meta:
+        db_table = "guests"
+        verbose_name = "Guest"
+        verbose_name_plural = "Guests"
+
